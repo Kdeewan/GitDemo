4803a982d96f27fd5fe23cb0de8ae5e49d35bdef
Kartik <kartik.deewan@gmail.com>
2022-08-03 15:05:33 +0530

    First commit

---
+++ src/main/java/com/example/demo/SimpleController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+	@GetMapping("/get")
+	public String getSomething()
+	{
+		return "Hi Kartik, You are on..";
+	}
+}
